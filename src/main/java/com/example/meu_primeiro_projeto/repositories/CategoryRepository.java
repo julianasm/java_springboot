5ec38e08be10a9e38001461a588f2103e93acc3b
Juliana <julianasouzamelo@live.com>
2021-11-12 11:19:11 -0300

    criando projeto em springboot

---
+++ src/main/java/com/example/meu_primeiro_projeto/repositories/CategoryRepository.java
@@ -0,0 +1,27 @@
+package com.example.meu_primeiro_projeto.repositories;
+
+import com.example.meu_primeiro_projeto.entities.Category;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Component
+public class CategoryRepository {
+
+    private Map<Long, Category> map = new HashMap<>();
+
+    public void save(Category obj) {
+        map.put(obj.getId(), obj);
+    }
+
+    public Category findbyId(Long id) {
+        return map.get(id);
+    }
+
+    public List<Category> findAll() {
+        return new ArrayList<Category>(map.values());
+    }
+}
