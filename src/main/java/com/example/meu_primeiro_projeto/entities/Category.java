496bfb8dcf8b2747ccb3e1986815330a885f2dd4
Juliana <julianasouzamelo@live.com>
2021-11-12 15:09:43 -0300

    product memory

--- src/main/java/com/example/meu_primeiro_projeto/entities/Category.java
+++ src/main/java/com/example/meu_primeiro_projeto/entities/Category.java
@@ -1,18 +1,25 @@
 package com.example.meu_primeiro_projeto.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 public class Category  implements Serializable {
     private static final long serialVersionUID = 1L;
 
     private Long id;
-    private String nome;
+    private String name;
 
-    public Category(Long id, String nome) {
+    @JsonIgnore
+    private List<Product> products = new ArrayList<>();
+
+    public Category(Long id, String name) {
         super();
         this.id = id;
-        this.nome = nome;
+        this.name = name;
     }
 
     public Long getId() {
@@ -24,11 +31,15 @@ public class Category  implements Serializable {
     }
 
     public String getNome() {
-        return nome;
+        return name;
     }
 
     public void setNome(String nome) {
-        this.nome = nome;
+        this.name = nome;
+    }
+
+    public List<Product> getProducts() {
+        return products;
     }
 
     @Override
---
+++ src/main/java/com/example/meu_primeiro_projeto/resource/ProductResource.java
@@ -0,0 +1,32 @@
+package com.example.meu_primeiro_projeto.resource;
+
+import com.example.meu_primeiro_projeto.entities.Product;
+import com.example.meu_primeiro_projeto.repositories.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/products")
+public class ProductResource {
+
+    @Autowired
+    private ProductRepository categoryRepository;
+
+    @GetMapping
+    public ResponseEntity<List<Product>> findAll() {
+        List<Product> list = categoryRepository.findAll();
+        return ResponseEntity.ok().body(list);
+    }
+
+   @GetMapping(value = "/{id}")
+    public ResponseEntity<Product> findById(@PathVariable Long id) {
+        Product cat = categoryRepository.findbyId(id);
+        return ResponseEntity.ok().body(cat);
+    }
+}
